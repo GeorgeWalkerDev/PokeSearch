b64aaeac3d9529d9b36b7e0f2d16ee54c3b3ff7f
Ge0h <george.walker.wme@gmail.com>
2022-10-23 10:19:34 +0100

    Pokemon search with name, index, type and weight details included

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,17 +1,30 @@
 document.querySelector('#searchPokemon').addEventListener('submit', getPokemon);
 
+//Fetch pokemon data from user input
 function getPokemon(e) {
     e.preventDefault();
-    const pokeInput = document.querySelector('#pokeInput').value;
+    //Get user input
+    const pokeInput = document.querySelector('#pokeInput').value.toLowerCase();
+    //Fetch data
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokeInput}/`)
         .then(res => res.json())
         .then(data => {
-            console.log(data);
+            //Render DOM
             renderResults(data);
         })
-        .catch(err => console.log(`Error: ${err}`))
+        .catch(err => {
+            //Render no results warning if nothing found
+            const searchResults = document.querySelector('#searchResults');
+            searchResults.textContent = '';
+            const noneFound = document.createElement('h2');
+            noneFound.textContent = 'No search results found please try again.';
+            searchResults.append(noneFound);
+            //Log error
+            console.log(`Error: ${err}`)
+        })
 }
 
+//Render DOM with fetched results
 function renderResults(result) {
     document.querySelector('#searchResults').textContent = ''
     const divContainer = document.createElement('div');
@@ -24,13 +37,32 @@ function renderResults(result) {
     image.src = result.sprites.other['official-artwork']['front_default']
     const header = document.createElement('header');
     const h3 = document.createElement('h3');
-    h3.textContent = result.name.charAt(0).toUpperCase() + result.name.slice(1);
-    const type = document.createElement('p');
+    // Assign text content of header (with first letter at uppercase)
+    h3.textContent = firstCharToUpperCase(result.name);
+    const h4 = document.createElement('h4');
+    h4.textContent = `#${result['game_indices'][3]['game_index']}`;
     const weight = document.createElement('p');
     weight.textContent = `Weight: ${result.weight}`
     document.querySelector('#searchResults').append(divContainer);
     divContainer.append(sectionBox);
     link.append(image);
-    header.append(h3);
-    sectionBox.append(link, header, type, weight);
+    header.append(h3, h4);
+    const type = document.createElement('p');
+    type.textContent = 'Type: '
+    sectionBox.append(link, header, type);
+    // Loop through types to render to DOM
+    result.types.forEach((el, i) => {
+        if (i === 0) {
+            type.textContent += `${firstCharToUpperCase(el.type.name)}`;
+        } else {
+            type.textContent += `, ${firstCharToUpperCase(el.type.name)}`;
+        }
+    })
+    sectionBox.append(weight);
+}
+
+
+//First character of string to upper case helper function
+function firstCharToUpperCase(str){
+    return str.charAt(0).toUpperCase() + str.slice(1);
 }
